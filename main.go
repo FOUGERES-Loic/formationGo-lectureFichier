15c8a424d56ed0ba25cfa6e4dbb268c7d7f58ced
Loïc FOUGERES <loic.fougeres@smile.fr>
2022-07-26 11:03:36 +0200

    initial commit

---
+++ main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+)
+
+func readFile(filename string) (string, error) {
+	dat, err := ioutil.ReadFile(filename)
+	if err != nil {
+		return "", err
+	}
+
+	if len(dat) == 0 {
+		//return "", errors.New("empty content")
+		return "", fmt.Errorf("empty content (filename: %v)", filename)
+	}
+
+	return string(dat), nil
+}
+
+func main() {
+	dat, err := readFile("test.txt")
+	if err != nil {
+		fmt.Printf("Error while reading file: %v\n", err)
+		return
+	}
+
+	fmt.Println("File content:")
+	fmt.Println(dat)
+
+}
